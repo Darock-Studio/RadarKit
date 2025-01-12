2592e962707e5ce742958d2d97b9234693b1aec7
WindowsMEMZ <memz1@darock.top>
2025-01-13 00:46:20 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RadarKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "RadarKit", targets: ["RadarKit", "_RadarKitDependenciesWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Darock-Studio/RadarKitCore", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "RadarKit", path: "RadarKit.xcframework"),
+        .target(name: "_RadarKitDependenciesWrapper", dependencies: [
+            "RadarKitCore"
+        ])
+    ]
+)
